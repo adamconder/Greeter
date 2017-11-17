096e4108d0ba2f597c784a8032a5f23367c870ba
Adam Conder <adamdconder@gmail.com>
2017-11-17 10:52:58 +0000

    Add mixin using traits inheritance

--- src/main/scala/app/GreeterApplication.scala
+++ src/main/scala/app/GreeterApplication.scala
@@ -8,21 +8,37 @@ object Prompt {
 
 }
 
-class Person(name : String) {
+trait Talking {
+
+  protected def sayHello : String
+
+}
+
+trait English extends Talking {
+  override protected def sayHello: String = "Hello"
+}
+
+trait French extends Talking {
+  override protected def sayHello: String = "Salut"
+}
+
+abstract class Person(name : String) {
+  this : Talking =>
 
   def speak() : String = {
     if (name.toLowerCase.contains("adam")) {
       s"You don't get a hello!"
     } else {
-      s"Hello $name"
+      s"$sayHello $name"
     }
   }
 
 }
 
+class Tutor(name : String) extends Person(name) with English
+
 object GreeterApplication extends App {
   val name = Prompt.ask("What is your name? ")
-  val person = new Person(name)
+  val person = new Tutor(name)
   println(person.speak())
 }
-
